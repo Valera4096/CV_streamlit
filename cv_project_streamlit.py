45dda089fe888d01acfc31029be859818a4adf30
Valera <Fyodorov.val99@gmail.com>
2024-03-01 00:21:55 +0300

    добавил детект по url

--- cv_project_streamlit.py
+++ cv_project_streamlit.py
@@ -3,26 +3,29 @@ import torch
 from PIL import  Image
 import cv2
 import numpy as np
-
+######
+import requests
+from io import BytesIO
 
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', 'best.torchscript', trust_repo=True) 
 model.eval()
 model.conf = 0.8
 
-
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-if uploaded_file is not None:
-    # Преобразуйте изображение в формат OpenCV
-    image = Image.open(uploaded_file)
-    image_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-    # Выполните детекцию объектов с помощью модели YOLOv5
+def prediction(input, url_reques = False):
+    if url_reques:
+        response = requests.get(url)
+        image = Image.open(BytesIO(response.content))
+        image_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    else:
+        image = Image.open(input)
+        image_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    
+    
+    image_cv2 = cv2.resize(image_cv2, (540, 540))
+        # Выполните детекцию объектов с помощью модели YOLOv5
     results = model(image_cv2)
 
-    
     results_image = Image.fromarray(results.render()[0])
 
     # Отобразите оригинальное изображение и результаты детекции
@@ -30,7 +33,6 @@ if uploaded_file is not None:
     st.image(image)
 
     st.subheader('Результат детекций')
-
     st.image(results_image)
 
     detections = results.xyxy[0]
@@ -38,4 +40,20 @@ if uploaded_file is not None:
         class_id = int(detection[5])
         class_name = model.names[class_id]
         confidence = float(detection[4])
-        st.write(f'Обнаруженный класс {class_name}: С вероятностью {confidence:.2f} %')
+        st.write('='*88)  
+        st.write(f'Обнаруженный класс {class_name}') 
+        st.write(f'С вероятностью {confidence:.2f} %')
+        st.write('='*88)  
+
+
+uploaded_file2 = st.file_uploader("Загрузите изображение", type=["jpg",'png'], accept_multiple_files=True)
+if uploaded_file2 is not None:
+    for i in uploaded_file2:
+        prediction(i)
+        
+                        
+url = st.text_input("Введите URL изображения:")
+
+if url:
+    prediction(url, url_reques= True)
+        
